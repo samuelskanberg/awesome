ca47f055df448392ca2b2d69e24b85685f8a2d4b
Samuel Skanberg <samuel.skanberg@gmail.com>
2017-06-30 18:38:52 +0200

    Add mul function

--- awesome.py
+++ awesome.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 
+import sys
+
 def add(a, b):
     return a+b
 
+def mul(a, b):
+    return a*b
+
 if __name__ == "__main__":
-    print("Hello world!")
+    a = int(sys.argv[1])
+    b = int(sys.argv[2])
+    total = add(a, b)
+    print("Total: {}".format(total))
--- test.py
+++ test.py
@@ -2,12 +2,15 @@
 
 import unittest
 
-from awesome import add
+from awesome import add, mul
 
 class AwesomeTest(unittest.TestCase):
     def test_add(self):
         total = add(1, 2)
         self.assertEqual(3, total)
+    def test_mul(self):
+        total = mul(2, 3)
+        self.assertEqual(6, total)
 
 
 if __name__ == "__main__":
