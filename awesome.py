eb65eb63871fe7a145609e54ef714222b4595381
Samuel Skanberg <samuel.skanberg@gmail.com>
2017-06-30 18:50:43 +0200

    Some comments

--- awesome.py
+++ awesome.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python3
 
+"""
+This is an awesome script
+"""
+
 import sys
 
+"""
+Will add two numbers
+"""
 def add(a, b):
     return a+b
 
+"""
+Will multiply two numbers
+"""
 def mul(a, b):
     return a*b
 
