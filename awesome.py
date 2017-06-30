361af511064b454720cb809caa44cef7c2710dcb
Samuel Skanberg <samuel.skanberg@gmail.com>
2017-06-30 18:43:41 +0200

    Add sub function

--- awesome.py
+++ awesome.py
@@ -3,5 +3,8 @@
 def add(a, b):
     return a+b
 
+def sub(a, b):
+    return a-b
+
 if __name__ == "__main__":
     print("Hello world!")
--- test.py
+++ test.py
@@ -2,12 +2,15 @@
 
 import unittest
 
-from awesome import add
+from awesome import add, sub
 
 class AwesomeTest(unittest.TestCase):
     def test_add(self):
         total = add(1, 2)
         self.assertEqual(3, total)
+    def test_sub(self):
+        total = sub(4, 2)
+        self.assertEqual(2, total)
 
 
 if __name__ == "__main__":
